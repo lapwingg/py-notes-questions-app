06f5c979b24f22f9c6917a05ed801bf668f5fdff
lapwingg <kamilczajka2@gmail.com>
2021-01-01 13:13:29 +0100

    Updated README.md, and commit hah in the app

--- src/presentation/info/info_view_model.py
+++ src/presentation/info/info_view_model.py
@@ -6,7 +6,7 @@ class InfoViewModel:
     """View model supports Info view"""
     developer = 'lapwingg'
     app_version = '0.1'
-    commit_hash = 'c4b33f9'
+    commit_hash = '4601b64d'
     py_qt_version = '5.15.2'
     python_version = '3.9'
 
